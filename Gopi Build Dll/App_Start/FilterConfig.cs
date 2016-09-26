0bd4b3623b67d49e87c30a093bf7b334e4b865f6
123Caddy <gopimunnangi@gmail.com>
2016-09-26 09:03:26 +0530

    First commit

---
+++ Gopi Build Dll/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Gopi_Build_Dll
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Gopi Build Dll/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Gopi_Build_Dll.MvcApplication" Language="C#" %>
